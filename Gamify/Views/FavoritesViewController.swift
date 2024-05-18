f59ec71a943dfb2e94d0ffe0e58110f7552f32ee
Necati Alperen Işık <necatialperen1@icloud.com>
2024-05-18 04:25:20 +0300

    setup tabbar

---
+++ Gamify/Views/FavoritesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesViewController.swift
+//  Gamify
+//
+//  Created by Necati Alperen IŞIK on 18.05.2024.
+//
+
+import UIKit
+
+final class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    
+
+}
---
+++ Gamify/Views/MainTabbarController.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabbarController.swift
+//  Gamify
+//
+//  Created by Necati Alperen IŞIK on 18.05.2024.
+//
+
+import UIKit
+
+final class MainTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabbar()
+    }
+    
+    private func setupTabbar() {
+        tabBar.backgroundColor = .systemGray3
+        let vcFirst  = UINavigationController(rootViewController:HomeViewController())
+        let vcSecond = UINavigationController(rootViewController:FavoritesViewController())
+        vcFirst.tabBarItem.image = UIImage(systemName: "gamecontroller")
+        vcSecond.tabBarItem.image = UIImage(systemName: "heart")
+        vcFirst.tabBarItem.title = "Games"
+        vcSecond.tabBarItem.title = "Favorites"
+        tabBar.tintColor = .label
+        setViewControllers([vcFirst,vcSecond], animated: true)
+    }
+
+    
+
+}
